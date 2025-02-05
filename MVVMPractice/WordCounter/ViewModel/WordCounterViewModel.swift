074975a932677200103efa86f6e51883ea5270b3
Park Junwoo <ays99111@naver.com>
2025-02-05 21:03:43 +0900

    [feat] WordCounterViewModel 구현

---
+++ MVVMPractice/WordCounter/ViewModel/WordCounterViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  WordCounterViewModel.swift
+//  MVVMPractice
+//
+//  Created by 박준우 on 2/5/25.
+//
+
+import Foundation
+
+final class WordCounterObserver<T> {
+    var value: T {
+        didSet {
+            self.closure?(value)
+        }
+    }
+    
+    var closure: ((T) -> ())?
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> ()) {
+        closure(self.value)
+        self.closure = closure
+    }
+}
+
+final class WordCounterViewModel {
+    
+    var inputData: WordCounterObserver<String?> = WordCounterObserver(value: nil)
+    
+    var outputData: WordCounterObserver<String> = WordCounterObserver(value: "현재까지 0글자 작성중")
+    
+    init() {
+        self.inputData.closure = { text in
+            self.countWords()
+        }
+    }
+    
+    private func countWords() {
+        guard let inputString = self.inputData.value else {
+            self.outputData.value = "현재까지 0글자 작성중"
+            return
+        }
+        self.outputData.value = "현재까지 \(inputString.count)글자 작성중"
+    }
+}
