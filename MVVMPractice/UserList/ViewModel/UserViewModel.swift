13ce1155cd8b8e0db1258e972e2d71ee61775e24
Park Junwoo <ays99111@naver.com>
2025-02-05 21:39:06 +0900

    [feat] UserViewModel 구현

--- MVVMPractice/UserList/ViewModel/UserViewModel.swift
+++ MVVMPractice/UserList/ViewModel/UserViewModel.swift
@@ -7,6 +7,62 @@
 
 import Foundation
 
-final class UserViewModel {
+final class UserObserver<T> {
+    var value: T {
+        didSet {
+            self.closure?(self.value)
+        }
+    }
     
+    var closure: ((T) -> ())?
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> ()) {
+        self.closure = closure
+    }
+}
+
+final class UserViewModel {
+    let loadInput: UserObserver<Void> = UserObserver(value: ())
+    
+    let resetInput: UserObserver<Void> = UserObserver(value: ())
+    
+    let addInput: UserObserver<Void> = UserObserver(value: ())
+    
+    let outputData: UserObserver<[Person]> = UserObserver(value: [])
+    
+    init() {
+        self.loadInput.closure = { _ in
+            self.loadDatas()
+        }
+        self.resetInput.closure = { _ in
+            self.resetData()
+        }
+        self.addInput.closure = { _ in
+            self.addData()
+        }
+    }
+    
+    private func loadDatas() {
+        self.outputData.value = [
+            Person(name: "James", age: Int.random(in: 20...70)),
+            Person(name: "Mary", age: Int.random(in: 20...70)),
+            Person(name: "John", age: Int.random(in: 20...70)),
+            Person(name: "Patricia", age: Int.random(in: 20...70)),
+            Person(name: "Robert", age: Int.random(in: 20...70))
+        ]
+    }
+    
+    private func resetData() {
+        self.outputData.value.removeAll()
+    }
+    
+    private func addData() {
+        let names = ["James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth", "David", "Barbara", "Richard", "Susan", "Joseph", "Jessica", "Thomas", "Sarah", "Charles", "Karen"]
+        let user = Person(name: names.randomElement()!, age: Int.random(in: 20...70))
+        self.outputData.value.append(user)
+    }
 }
